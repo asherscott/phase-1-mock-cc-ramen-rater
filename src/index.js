554e416a929966fa1c6f9eaaa46fa4522c5c589e
asher scott <asher.scott6@gmail.com>
2022-03-03 12:35:17 -0600

    cleaned up code

--- src/index.js
+++ src/index.js
@@ -1,85 +1,61 @@
 // write your code here
+const ramenMenuDiv  = document.querySelector('#ramen-menu');
+const detailImg     = document.querySelector('.detail-image');
+const nameH2        = document.querySelector('.name');
+const restH3        = document.querySelector('.restaurant');
+const rateP         = document.querySelector('#rating-display');
+const commentP      = document.querySelector('#comment-display');
+const form          = document.querySelector('form');
+const inputs        = document.querySelectorAll('input');
+const commentSub    = document.querySelector('textarea');
+
+
+
 fetch('http://localhost:3000/ramens')
 .then(res => res.json())
 .then(json => printImages(json))
 
-const ramenMenuDiv = document.querySelector('#ramen-menu');
-const detailImg = document.querySelector('.detail-image');
-const nameH2 = document.querySelector('.name');
-const restH3 = document.querySelector('.restaurant');
-const rateP = document.querySelector('#rating-display');
-const commentP = document.querySelector('#comment-display');
 
-const form = document.querySelector('form');
-const inputs = document.querySelectorAll('input');
-const commentSub = document.querySelector('textarea');
-
-// console.log(ramenMenuDiv)
 
 function printImages(array) {
-    // console.log(array)
-    array.forEach((obj) => {
-        // console.log(obj)
 
+
+    // DELIVERABLE 1
+    array.forEach((obj) => {
         const img = document.createElement('img');
         img.src = obj.image;
         ramenMenuDiv.append(img);
 
+        // DELIVERABLE 2
         img.addEventListener('click', () => {
-            // console.log(obj.name)
             detailImg.src = img.src
             nameH2.textContent = obj.name
             restH3.textContent = obj.restaurant
-
             rateP.textContent = obj.rating
             commentP.textContent = obj.comment
         })
     })
-    // after submit
-    // make new obj
-    // append new img
-    // should add to EventListener
-    console.log(inputs)
-    let newRamen = {};
-    function newRamenFunc() {
-        form.addEventListener('submit', (event) => {
-            event.preventDefault()
 
-            // console.log(commentSub)
 
-            newRamen = {
-                name:        inputs[0].value,
-                restaurant:  inputs[1].value,
-                image:       inputs[2].value,
-                rating:      inputs[3].value,
-                comment:     commentSub.value,
-            }
+    // DELIVERABLE 3
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
 
-            console.log(newRamen)
-            console.log(newRamen.image)
-            // removes 'Submit' from the input button !!FIX!!
-            inputs.forEach(ele => ele.value = '')
+        let newRamen = {
+            name:        inputs[0].value,
+            restaurant:  inputs[1].value,
+            image:       inputs[2].value,
+            rating:      inputs[3].value,
+            comment:     commentSub.value,
+        }
 
-            // add to img div
-            const newImg = document.createElement('img');
-            newImg.src = newRamen.image;
-            newImg.alt = 'new Image'
-            ramenMenuDiv.append(newImg);
-            console.log(newImg)
 
-            newImg.addEventListener('click', () => {
-                // console.log(obj.name)
-                // detailImg.src = newImg.src
-                // nameH2.textContent = obj.name
-                // restH3.textContent = obj.restaurant
-    
-                // rateP.textContent = obj.rating
-                // commentP.textContent = obj.comment
-            })
+        const newInputs = Array.from(inputs).slice(0, 3);
+        newInputs.forEach(ele => ele.value = '')
 
-            
-
-        })
-    }
-    newRamenFunc()
+        const newImg = document.createElement('img');
+        newImg.src = newRamen.image;
+        newImg.alt = 'new image'
+        ramenMenuDiv.append(newImg);
+    })
 }
